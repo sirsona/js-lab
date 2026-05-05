0779cf74add8d825abcee98de504608dacf252dd
sirsona <swaleh489@gmail.com>
2026-05-05 22:03:12 +0300

    feat: render cards dynamically

---
+++ week07/day2/react-lab/src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+import Card from "./components/Card";
+import Container from "./components/Container";
+
+const cards = [
+  { id: 1, title: "Home", body: "Welcome home" },
+  { id: 2, title: "About", body: "About us" },
+  { id: 3, title: "Contact", body: "Get in touch" },
+];
+function App() {
+  return (
+    <>
+      {/* <Container>
+        <Card title="Welcome">
+          <p>This is the first card.</p>
+          <Button>Click me</Button>
+        </Card>
+        <Card title="About">
+          <p>This is the second card.</p>
+          <Button>More info</Button>
+        </Card>
+      </Container> */}
+
+      <Container>
+        {cards.map((card) => (
+          <Card key={card.id} title={card.title}>
+            <p>{card.body}</p>
+          </Card>
+        ))}
+      </Container>
+    </>
+  );
+}
+
+export default App;
