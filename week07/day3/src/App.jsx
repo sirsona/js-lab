a2535c936fc633c0b7319f555cf13cad387c06ed
sirsona <swaleh489@gmail.com>
2026-05-08 18:08:06 +0300

    feat: render counter and toggle buttons

---
+++ week07/day3/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Counter from "./components/Counter";
+import Toggle from "./components/Toggle";
+
+function App() {
+  return (
+    <>
+      <Counter />
+
+      <Toggle />
+    </>
+  );
+}
+
+export default App;
