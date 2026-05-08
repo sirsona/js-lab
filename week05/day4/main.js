7054e9d1f77627154a64c19accb297ad31121041
sirsona <swaleh489@gmail.com>
2026-05-08 15:03:49 +0300

    fix: add missing imports

--- week05/day4/main.js
+++ week05/day4/main.js
@@ -1,19 +1,27 @@
-
 // main.js
 
+import settings from "./account/defaults.js";
+import { formatCurrency } from "./account/format.js";
 import { SavingsAccount } from "./account/index.js";
-
 
 const s = new SavingsAccount("Wanjiru", 500000, 0.08);
 s.deposit(100000);
 s.addInterest();
-s.balanceCent
+s.balanceCents;
 
-s.transcations;
-
+s.transactions;
 
 import("./account/index.js").then((mod) => {
-    const acc = new mod.BankAccount("John", 100000);
-    acc.deposit(50000);
-    console.log(acc.balance);
+  const acc = new mod.BankAccount("John", 100000);
+  acc.deposit(50000);
+  console.log(acc.balance);
 });
+
+// formatCurrency
+
+console.log(formatCurrency(25999));
+
+// default.js call
+
+console.log(settings.minBalance);
+console.log(settings.interestRate);
