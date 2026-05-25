9884239c814ebb6f00c9b2b5d9dce874144a4c10
sirsona <swaleh489@gmail.com>
2026-05-25 10:07:03 +0300

    feat: add local storage

---
+++ week08/day2/src/components/TaskManger.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+
+export function TaskManager() {
+  const [tasks, setTasks] = useState(() => {
+    const saved = localStorage.getItem("tasks");
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  const [input, setInput] = useState("");
+
+  function addTask() {
+    if (!input.trim()) return;
+    setTasks([...tasks, { id: Date.now(), text: input, completed: false }]);
+
+    setInput("");
+  }
+
+  function toggleTask(id) {
+    setTasks(
+      tasks.map((t) => (t.id === id ? { ...t, completed: !t.completed } : t)),
+    );
+  }
+
+  function deleteTask(id) {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
+  // useEffect(() => {
+  //   const saved = localStorage.getItem("tasks");
+
+  //   if (saved) {
+  //     setTasks(JSON.parse(saved));
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  return (
+    <>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={addTask}>Add</button>
+      {tasks.length === 0 ? (
+        <p>No tasks yet -- add you first one above</p>
+      ) : (
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>
+              <button
+                onClick={() => toggleTask(task.id)}
+                style={{
+                  textDecoration: task.completed ? "line-through" : "none",
+                }}
+              >
+                {task.text}
+              </button>
+              <button onClick={() => deleteTask(task.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      )}
+      <p>{tasks.filter((t) => !t.completed).length} task(s) remaining</p>
+    </>
+  );
+}
