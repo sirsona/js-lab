35a22fecb925e4fabe290138572d55fece3e2e8e
sirsona <swaleh489@gmail.com>
2026-05-28 15:14:08 +0300

    feat: render UserList component

--- week09/day1/src/App.jsx
+++ week09/day1/src/App.jsx
@@ -3,11 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
+import UserList from "./components/UserList";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <UserList />
+    </>
+  );
 }
 
 export default App;
