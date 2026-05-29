9d74d99583d7044523e3c2c8ca26c291ef4c79ab
sirsona <swaleh489@gmail.com>
2026-05-29 16:49:50 +0300

    feat: apply global theme context using useTheme()

--- week09/day2/src/components/TemperatureDisplay.jsx
+++ week09/day2/src/components/TemperatureDisplay.jsx
@@ -1,7 +1,11 @@
+import { useTheme } from "./ThemeContext.jsx";
+
 function TemperatureDisplay({ celsius }) {
   const fahrenheit = (celsius * 9) / 5 + 32;
+  const { theme, setTheme } = useTheme();
   return (
     <div>
+      <h2>Theme is: {theme}</h2>
       <p>Celsius: {celsius.toFixed(1)}</p>
       <p>Fahrenheit: {fahrenheit.toFixed(1)}</p>
     </div>
