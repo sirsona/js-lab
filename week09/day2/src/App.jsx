a5e7cec24539f5af8d0dc3a2f27b6427d141b659
sirsona <swaleh489@gmail.com>
2026-05-29 16:51:47 +0300

    refactor

--- week09/day2/src/App.jsx
+++ week09/day2/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
+import TemperaturePage from "./components/TemperaturePage";
+import ThemeToggle from "./components/ThemeToggle";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <div>
+      <ThemeToggle />
+      <TemperaturePage />
+    </div>
+  );
 }
 
 export default App;
--- week09/day2/src/components/TemperaturePage.jsx
+++ week09/day2/src/components/TemperaturePage.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import TemperatureInput from "./TemperatureInput";
 import TemperatureDisplay from "./TemperatureDisplay";
+import TemperatureInput from "./TemperatureInput";
 
 function TemperaturePage() {
   const [celsius, setCelsius] = useState(20);
   return (
     <div>
-      <TemperatureInput celsius={celsius} onChange={setCelsius} />
       <TemperatureDisplay celsius={celsius} />
+      <TemperatureInput celsius={celsius} onChange={setCelsius} />
     </div>
   );
 }
