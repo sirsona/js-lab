cdcd596755e7085d6acd7ed3d73a001f4310497d
sirsona <swaleh489@gmail.com>
2026-05-30 20:39:34 +0300

    refactor: memoize toggle function with useCallback

--- week09/day4/src/hooks/useToggle.js
+++ week09/day4/src/hooks/useToggle.js
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export function useToggle(initial = false) {
     const [value, setValue] = useState(initial);
 
-    const toggle = () => setValue((v) => !v);
+    const toggle = useCallback(() => {
+        setValue((v) => (!v));
+    }, [setValue]);
 
     return [value, toggle];
 }
