5a744d2cefe9d934b003f6b6e271f21fd0ecb91a
sirsona <swaleh489@gmail.com>
2026-05-30 17:46:15 +0300

    fix: add dedicated Vitest configuration file

--- week09/day4/vite.config.js
+++ week09/day4/vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: "jsdom",
-    global: true,
-    setupFiles: "./src/test-setup.js",
-  }
-})
+
+});
