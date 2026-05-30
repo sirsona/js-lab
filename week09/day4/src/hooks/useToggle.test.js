e8148d95f59609495d571f9891f39938d72f0625
sirsona <swaleh489@gmail.com>
2026-05-30 20:31:43 +0300

    fix: typo

--- week09/day4/src/hooks/useToggle.test.js
+++ week09/day4/src/hooks/useToggle.test.js
@@ -22,24 +22,3 @@ describe("useToggle", () => {
 
 });
 
-// import { act, renderHook } from "@testing-library/react";
-// import { describe, expect, it } from "vitest";
-// import { useToggle } from "./useToggle";
-
-// describe("useToggle", () => {
-//     it("starts at false by default", () => {
-//         const { result } = renderHook(() => useToggle());
-
-//         expect(result.current[0]).toBe(false);
-//     });
-
-//     it("toggles to true on call", () => {
-//         const { result } = renderHook(() => useToggle());
-
-//         act(() => {
-//             result.current[1]();
-//         });
-
-//         expect(result.current[0]).toBe(true);
-//     });
-// });
