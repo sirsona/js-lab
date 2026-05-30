26b7e0a2245f1712def4caf386287ec69703c109
sirsona <swaleh489@gmail.com>
2026-05-30 17:47:43 +0300

    fix: typo

--- week09/day4/src/utils/format.js
+++ week09/day4/src/utils/format.js
@@ -1,8 +1,4 @@
-// export function formatCurrency(cents) {
 
-//     return `KSh ${cents / 100}`;
-
-// }
 export function formatCurrency(cents) {
     return `KSh ${new Intl.NumberFormat().format(cents / 100)}`;
 }
