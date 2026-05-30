7d31cbd8fe88d7323d839c0582aa2eba2477f127
sirsona <swaleh489@gmail.com>
2026-05-30 20:07:05 +0300

    test: add useToggle hook tests

---
+++ week09/day4/src/hooks/useToggle.test.js
@@ -0,0 +1,45 @@
+
+import { act, renderHook } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import { useToggle } from "./useToggle";
+
+
+
+
+describe("useToggle", () => {
+    it("starts at false by default", () => {
+        const { result } = renderHook(() => useToggle());
+        expect(result.current[0]).toBe(false);
+    });
+
+    it("toggle to true on call", () => {
+        const { result } = renderHook(() => useToggle());
+        act(() => {
+            result.current[1]();
+        });
+        expect(result.current[0]).toBe(true);
+    });
+
+});
+
+// import { act, renderHook } from "@testing-library/react";
+// import { describe, expect, it } from "vitest";
+// import { useToggle } from "./useToggle";
+
+// describe("useToggle", () => {
+//     it("starts at false by default", () => {
+//         const { result } = renderHook(() => useToggle());
+
+//         expect(result.current[0]).toBe(false);
+//     });
+
+//     it("toggles to true on call", () => {
+//         const { result } = renderHook(() => useToggle());
+
+//         act(() => {
+//             result.current[1]();
+//         });
+
+//         expect(result.current[0]).toBe(true);
+//     });
+// });
